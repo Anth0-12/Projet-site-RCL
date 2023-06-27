d474f4ef1ec00b29795c84ed7ef7e4a1ae5d37ac
Anth0-12 <antho62680@live.fr>
2023-06-27 22:29:13 +0200

    site RCL

---
+++ js/menuDeroulant.js
@@ -0,0 +1,9 @@
+document.getElementById("hamburger").onclick = function() {deroulerMenu(), rotation()};
+
+function deroulerMenu() {
+    document.getElementById("menuDeroulant").classList.toggle("show");
+}
+
+
+
+    
