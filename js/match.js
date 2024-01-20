a9782300e77c3589dc3ed4be25e863c626df9597
Anth0-12 <antho62680@live.fr>
2024-01-21 00:06:16 +0100

    Suppression du fichier

--- js/match.js
+++
@@ -1,6 +0,0 @@
-$('input[type="checkbox"]').change(function(){
-    console.log($(this).is(':checked'));
-    if($(this).is(':checked')){
-      $(this).siblings('input[type="checkbox"]').attr('checked', false);
-    }
-  });
